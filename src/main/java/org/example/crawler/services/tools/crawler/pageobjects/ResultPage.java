2992e4651c4e99eb5b35d0d2c151842bc12121ac
Gennady Sitnikov <Gennady_Sitnikov@genpt.com>
2024-05-31 00:34:21 +0200

    Update resultPage page object.

--- src/main/java/org/example/crawler/services/tools/crawler/pageobjects/ResultPage.java
+++ src/main/java/org/example/crawler/services/tools/crawler/pageobjects/ResultPage.java
@@ -1,6 +1,7 @@
 package org.example.crawler.services.tools.crawler.pageobjects;
 
 import com.microsoft.playwright.Page;
+import java.util.ArrayList;
 import java.util.List;
 import org.example.crawler.web.model.FoundModelDto;
 
@@ -18,7 +19,7 @@ public class ResultPage {
     public List<FoundModelDto> getLots() {
 
         Integer maxPage = pager.getMaxPageNo();
-        List<LotElement> lots = page.locator(lotCss).all().stream().map(LotElement::new).toList();
+        List<LotElement> lots = new ArrayList<>(page.locator(lotCss).all().stream().map(LotElement::new).toList());
         for (int i = 2; i <= maxPage; i++) {
             pager.openPage(i);
             lots.addAll(page.locator(lotCss).all().stream().map(LotElement::new).toList());
