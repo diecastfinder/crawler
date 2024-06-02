1a8865461ed1cc128271a578b2723013961aa3fa
Gennady Sitnikov <Gennady_Sitnikov@genpt.com>
2024-06-02 02:01:53 +0200

    Add implementation of controller and fixes for crawler.

--- src/main/java/org/example/crawler/services/tools/crawler/Crawler.java
+++ src/main/java/org/example/crawler/services/tools/crawler/Crawler.java
@@ -4,6 +4,8 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import java.util.List;
+import java.util.Objects;
+import org.example.crawler.services.tools.crawler.pageobjects.ResultPage;
 import org.example.crawler.web.model.FoundModelDto;
 import org.example.crawler.web.model.WantedModelDto;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,13 +21,17 @@ public class Crawler {
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch();
             Page page = browser.newPage();
-
-
             page.navigate(BASE_URL + URL_PREFIX + wanted.getName());
-            System.out.println(page.title());
-        }
 
+            ResultPage resultPage = new ResultPage(page);
+            List<FoundModelDto> lotsFoundByName = resultPage.getLots();
+            return lotsFoundByName.stream()
+                .filter(l -> Objects.nonNull(l.getPrice()) && l.getPrice() >= wanted.getMinPrice() && l.getPrice() <= wanted.getMaxPrice())
+                .filter(l -> Objects.isNull(l.getProducer()) || l.getProducer().equals(wanted.getProducer()))
+                .toList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
-
 }
--- src/main/java/org/example/crawler/web/api/controller/CrawlerController.java
+++ src/main/java/org/example/crawler/web/api/controller/CrawlerController.java
@@ -1,10 +1,29 @@
 package org.example.crawler.web.api.controller;
 
+import java.util.List;
+import org.example.crawler.services.CrawlerService;
+import org.example.crawler.web.model.FoundModelDto;
+import org.example.crawler.web.model.WantedModelDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("api/v1/crawler")
+@RequestMapping("api/v1/crawler/find")
 @RestController
 public class CrawlerController {
+    private final CrawlerService crawlerService;
+
+    public CrawlerController(CrawlerService crawlerService) {
+        this.crawlerService = crawlerService;
+    }
+
+    @PostMapping
+    public ResponseEntity findWantedModel(@RequestBody WantedModelDto wantedModelDto) {
+        List<FoundModelDto> foundModelDtos = crawlerService.findModel(wantedModelDto);
+
+        return ResponseEntity.ok(foundModelDtos);
+    }
 
 }
